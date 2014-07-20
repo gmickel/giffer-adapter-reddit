a5be896ded17243c147329e0a6c532be9cd4e965
Gordon Mickel <gm@cyon.ch>
2014-07-20 15:12:06 +0200

    Formatting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('inherits');
 var reddit = require('redwrap');
@@ -9,25 +10,25 @@ function Adapter(config) {
 
 }
 
-Adapter.prototype.start = function () {
-    this.emit('start');
-    // start grabbing some gifs
-    // and then `this.emit('gif', url)`
-    reddit.r('funny').sort('hot').limit(100, function(err, data, res){
-        console.log('errors:' + err);
-        //console.log(data);
-        /*_.forEach(data, function(post) {
-            console.log(post.children);
-        });*/
-        for (var children in data) {
-                console.log(children);
-            }
-    });
+Adapter.prototype.start = function() {
+  this.emit('start');
+  // start grabbing some gifs
+  // and then `this.emit('gif', url)`
+  reddit.r('funny').sort('hot').limit(100, function(err, data, res) {
+    console.log('errors:' + err);
+    //console.log(data);
+    /*_.forEach(data, function(post) {
+     console.log(post.children);
+     });*/
+    for (var children in data) {
+      console.log(children);
+    }
+  });
 };
 
-Adapter.prototype.stop = function () {
-    this.emit('stop');
-    // stop grabbing gifs
+Adapter.prototype.stop = function() {
+  this.emit('stop');
+  // stop grabbing gifs
 };
 
 module.exports = Adapter;
